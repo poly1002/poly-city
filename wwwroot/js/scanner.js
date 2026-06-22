33a9007b8f70b98be53c937a865cca97f0e0bc84
poly1002 <Karpich.2025@mail.ru>
2026-06-22 21:28:39 +0600

    первый релиз

---
+++ wwwroot/js/scanner.js
@@ -0,0 +1,45 @@
+﻿let currentScanner = null;
+
+window.startQrScanner = function () {
+    if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+        alert("Ваш браузер не поддерживает камеру :(");
+        return;
+    }
+
+    const html5QrCode = new Html5Qrcode("reader");
+
+    html5QrCode.start(
+        { facingMode: "environment" },
+        {
+            fps: 10,
+            qrbox: { width: 250, height: 250 }
+        },
+        (decodedText) => {
+            html5QrCode.stop();
+            currentScanner = null;
+            window.location.href = decodedText;
+        },
+        (error) => {
+            console.log(error);
+        }
+    ).catch(err => {
+        alert("Ошибка камеры: " + err);
+    });
+
+    currentScanner = html5QrCode;
+};
+
+window.stopQrScanner = function () {
+    return new Promise((resolve) => {
+        if (currentScanner) {
+            currentScanner.stop().then(() => {
+                currentScanner = null;
+                resolve();
+            }).catch(() => {
+                resolve();
+            });
+        } else {
+            resolve();
+        }
+    });
+};
